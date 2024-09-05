98957320dd6ea9f86eb792a1bc0f8dbeef702f69
eliott norie <eliott.norie@gmail.com>
2024-09-05 17:50:25 +0200

    added User and Channel class, need to code member functions

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enorie <enorie@student.42.fr>              +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 21:40:47 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/09/05 15:33:43 by enorie           ###   ########.fr       */
+/*   Updated: 2024/09/05 17:49:59 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,42 @@
 namespace irc {
 
     class Channel {
-    public:
-		Channel(std::string name);
-		Channel(const Channel &cpy);
-		~Channel();
-		Channel	&operator=(const Channel &cpy);
-		int		sendToAll(std::string message);
+		public:
+			Channel();
+			Channel(Channel &cpy);
+			Channel &operator=(Channel &cpy);
+			~Channel();
 
-		std::string			getName();
-		std::string			getTopic();
-		std::string			getPassword();
-		std::vector<User &> getUsers(void) const;
-		std::vector<User &> getOperators(void) const;
-		std::vector<User &> getInvites(void) const;
-		bool				isLimit();
-		bool				isInvites();
-		bool				isTopicRestricted();
-
-		int		joinUser(User &user);
-		int		kickUser(User &user);
-        int		inviteUser(User &user);
-		void	setTopic(std::string newTopic);
-		void	setPassword(std::string newPassword);
-		void	changeLimit(unsigned int limit);
-		void	changeInvite();
-		void	changeTopicRestrict();
-
-	private:
-		Channel();
-        std::string _name;
-        std::string _topic;
-		std::string _channelPassword;
-
-		std::vector<User *>	_users;
-        std::vector<User *>	_operators;
-        std::vector<User *>	_invitedUsers;
-		unsigned int		_userLimit;
-		bool				_onInvite;
-		bool				_topicRestricted;
+			std::string			getName();
+			std::string			getTopic();
+			std::string			getPassword();
+			std::vector<User &> getUsers(void) const;
+			std::vector<User &> getOperators(void) const;
+			std::vector<User &> getInvitedUsers(void) const;
+			int					getClientLimit();
+			bool				isInvitOnly();
+			bool				isTopicRestricted();
+			void				setName(std::string name);
+			void				setTopic(std::string topic);
+			void				setPassword(std::string password);
+			void				setClientLimit(int limit);
+			void				changeInvitOnly();
+			void				changeTopicRestrict();
+			void				addOperators(User& user);
+			int					sendToAll(std::string message);
+			int 				joinUser(User& user);
+			int 				kickUser(User& user);
+			int 				inviteUser(User& user);
+   		private:
+			std::string 		_name;
+			std::string 		_topic;
+			std::string 		_channelPassword;
+			std::vector<User *> _users;
+			std::vector<User *> _operators;
+			std::vector<User *> _invitedUsers;
+			unsigned int		_clientLimit;
+			bool				_invitOnly;
+			bool				_topicRestriction;
     };
 }
 
