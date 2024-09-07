7652b7448271c2bfd6c4d6ba6e75081c9abdbacf
HADMARINE <joonlee031@live.com>
2024-09-07 14:12:14 +0200

    Update comment

--- includes/ACommand.hpp
+++ includes/ACommand.hpp
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 21:18:50 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/09/04 18:10:50 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/09/07 13:53:42 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ namespace irc {
     public:
         ACommand();
         ACommand(std::vector<std::string> params);
-        virtual int resolve(Ircserv & server, User * user) = 0; // 0 if success
+        virtual int resolve(Ircserv & server, User * user) = 0; // return 0 if success
         void setParams(std::vector<std::string> params);
         virtual std::vector<std::string> setParamsMiddleware(std::vector<std::string> params);
     protected:
