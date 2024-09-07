0238082deb5b6d4ede60938bc3edd1f4704f8849
HADMARINE <joonlee031@live.com>
2024-09-07 14:12:25 +0200

    Reuse code

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enorie <enorie@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 14:19:41 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/09/05 15:30:14 by enorie           ###   ########.fr       */
+/*   Updated: 2024/09/07 13:56:14 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,23 @@ static void signalHdlr(int sig) {
     std::cout << std::endl;
 }
 
+static void printUsage() {
+    std::cout << "Usage : ./ircserv <password> <port>" << std::endl;
+    std::cout << "\t<password> : non void string value\n\t<port> : number in range of 1 ~ 65535\n\n";
+    std::cout << "Happy hacking !" << std::endl;
+}
+
 int main(int argc, char **argv)
 {
     if (argc == 2 && strncmp(argv[1], "help", 5) == 0) {
-        std::cout << "Usage : ./ircserv <password> <port>" << std::endl;
-        std::cout << "\t<password> : non void string value\n\t<port> : number in range of 1 ~ 65535\n\n";
-        std::cout << "Happy hacking !" << std::endl;
+       printUsage();
         return 0;
     }
     std::cout << "ircserv launched" << std::endl;
     if (argc != 3)
     {
         std::cerr << "Error : invalid parameters\n" << std::endl;
-        std::cout << "Usage : ./ircserv <password> <port>" << std::endl;
-        std::cout << "\t<password> : non void string value\n\t<port> : number in range of 1 ~ 65535\n\n";
-        std::cout << "Happy hacking !" << std::endl;
+        printUsage();
         return 1;
     }
     try {
