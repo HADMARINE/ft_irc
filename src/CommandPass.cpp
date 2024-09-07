205da5676c8994720a32cf2bd17b8fbcb2cd7781
HADMARINE <joonlee031@live.com>
2024-09-07 15:56:23 +0200

    Update CommandPass

--- src/CommandPass.cpp
+++ src/CommandPass.cpp
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 13:43:42 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/09/07 14:11:02 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/09/07 15:53:07 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ namespace irc {
     int CommandPASS::resolve(Ircserv & server, User * user) {
         (void)server;
         // TODO : Verification of logged-on user reauth try 
-        if (user) {
+        if (user->getIsRegistered()) {
             throw AlreadyRegistered();
         }
         user->setPendingpassword(this->_params.at(0));
