5ff68d56c58292cbf9021f0791568bf87aff26a8
Baptiste <baptiste.faisy@gmail.com>
2024-09-10 17:50:21 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/CommandMode.cpp
+++ src/CommandMode.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   CommandMode.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bfaisy <bfaisy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 18:06:27 by bfaisy            #+#    #+#             */
-/*   Updated: 2024/09/10 09:43:04 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/09/10 17:49:42 by bfaisy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,49 @@ namespace irc {
 		std::string ss;
 
 		std::string channelName = this->_params.at(0);
-
+		std::string option = this->_params.at(1);
+		
+		
 		if (!channel->isOperator(user)) {
 			throw NoPrivileges();  
 		}
+		if (option == "+i")
+			return (channel->changeInviteOnly(true), 0);
+		if (option == "-i")
+			return (channel->changeInviteOnly(false), 0);
+		if (option == "+t")
+			return (channel->changeTopicRestrict(true), 0);
+		if (option == "-t")
+			return (channel->changeTopicRestrict(false), 0);
+		if (option == "+k")
+		{
+			std::string password = this->_params.at(2);
+			return (channel->SetPasswordRequired(true), channel->setPassword(password), 0);
+		}
+		if (option == "-k")
+			return (channel->SetPasswordRequired(false), 0);
+		if (option == "+o")
+		{
+			std::string user = this->_params.at(2);
+			User *userop;
+			userop = server->findUserByNickSafe(user);
+			return (channel->addOperator(userop), 0);
+		}
+		if (option == "-o")
+		{
+			std::string user = this->_params.at(2);
+			User *userop;
+			userop = server->findUserByNickSafe(user);
+			return (channel->removeOperator(userop), 0); // en DEV
+		}
+		if (option == "+l")
+		{
+			std::string nbr = this->_params.at(2);
+			return (channel->isUserLimit(true), channel->setUserLimit(std::atoi((nbr.c_str()))), 0);
+		}
+		if (option == "-l")
+			return (channel->isUserLimit(false), 0);
+		return 0;
 	}
 
 	std::vector<std::string> CommandMODE::setParamsMiddleware(std::vector<std::string> params)
