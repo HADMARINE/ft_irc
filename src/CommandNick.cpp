498ac79c9b1173996c8afcc2be53cde3b57c3164
HADMARINE <joonlee031@live.com>
2024-09-11 14:55:57 +0200

    Update command NICK

--- src/CommandNick.cpp
+++ src/CommandNick.cpp
@@ -6,21 +6,30 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 17:28:34 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/09/10 22:48:58 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/09/11 13:24:05 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_irc.hpp"
 
+
+/*
+
+TODO
+Nickname rules : 
+
+They MUST NOT contain any of the following characters: space (' ', 0x20), comma (',', 0x2C), asterisk ('*', 0x2A), question mark ('?', 0x3F), exclamation mark ('!', 0x21), at sign ('@', 0x40).
+They MUST NOT start with any of the following characters: dollar ('$', 0x24), colon (':', 0x3A).
+They MUST NOT start with a character listed as a channel type, channel membership prefix, or prefix listed in the IRCv3 multi-prefix Extension.
+They SHOULD NOT contain any dot character ('.', 0x2E).
+
+*/
 namespace irc {
     int CommandNICK::resolve(Ircserv * server, User * user) {
-        if (server->getPassword() != user->getPendingpassword()) {
-            throw PasswordMisMatch();
-        }
 
-       if (server->findUserByNick(this->_params.at(0))) {
+        if (server->findUserByNick(this->_params.at(0))) {
             throw NicknameInUse(this->_params.at(0));
-       }
+        }
         user->setNickname(this->_params.at(0));
         return 0;
     }
